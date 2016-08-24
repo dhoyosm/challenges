e10a0ad0478b7f8bc248d1f2068a6719886a0977
Daniel Hoyos <dhoyosm@gmail.com>
2016-08-24 08:02:37 -0500

    Instructions

--- src/challenges/CompareTheTriplets.java
+++ src/challenges/CompareTheTriplets.java
@@ -2,6 +2,31 @@ package challenges;
 
 import java.util.Scanner;
 
+/*
+Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from to for three categories: problem clarity, originality, and difficulty.
+
+We define the rating for Alice's challenge to be the triplet , and the rating for Bob's challenge to be the triplet .
+
+Your task is to find their comparison scores by comparing with , with , and with .
+
+    If , then Alice is awarded point.
+    If , then Bob is awarded point.
+    If , then neither person receives a point.
+
+Given and , can you compare the two challenges and print their respective comparison points?
+
+Input Format
+
+The first line contains space-separated integers, , , and , describing the respective values in triplet .
+The second line contains space-separated integers, , , and , describing the respective values in triplet .
+
+Constraints
+
+Output Format
+
+Print two space-separated integers denoting the respective comparison scores earned by Alice and Bob.
+*/
+
 public class CompareTheTriplets {
 
 	public static void main(String[] args) {
